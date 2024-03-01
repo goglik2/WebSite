23d94d5ae3c042717d02926f74ba490d89db7796
goglik2 <griga983@gmail.com>
2024-03-01 16:28:06 +0300

    buttons

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -17,6 +17,16 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Index2()
+    {
+        return View();
+    }
+    
+    public IActionResult Index3()
+    {
+        return View();
+    }
 
     public IActionResult Expon()
     {
